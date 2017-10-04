76fabc0b7b9e583890edfb8d6e9b6dde3207badf
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-10-04 11:04:55 -0500

    initial structure

---
+++ src/app/components/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: '../views/login.component.html'
+})
+export class LoginComponent {
+
+  title = 'Login app';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/models/login.ts
@@ -0,0 +1,6 @@
+export class Login{
+  constructor(
+    public user: number,
+    public password: string
+  ){}
+}
